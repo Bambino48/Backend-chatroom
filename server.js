f1083d466a2a62c556e556c9a30317200ca044ae
Ibrahim Bamba <ibamba366@gmail.com>
2025-09-11 00:23:52 +0000

    Ajout du lien frontend

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 const cors = require("cors");
 
 app.use(cors({
-    origin: "http://localhost:3000", // autorise ton front React
+    origin: "https://frontend-chatroom-three.vercel.app", // autorise ton front React
     credentials: true
 }));
 app.use(express.json()); // Parse JSON
@@ -43,7 +43,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://frontend-chatroom-three.vercel.app",
     },
 });
 
